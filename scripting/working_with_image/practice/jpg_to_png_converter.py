f6d7e3cd53f152882d894242db10f370da1f06bf
Jatin Goyal <goyaljatin344@gmail.com>
2026-06-08 23:02:43 +0530

    exercise improvement

--- scripting/working_with_image/practice/jpg_to_png_converter.py
+++ scripting/working_with_image/practice/jpg_to_png_converter.py
@@ -2,6 +2,7 @@ from PIL import Image
 import sys
 import os
 from pathlib import Path
+
 # print(f"path is: {os.getcwd()}")
 # source_dir = sys.argv[1]
 # dest_dir = sys.argv[2]
@@ -9,9 +10,9 @@ from pathlib import Path
 source_dir = "../pokedex/"
 dest_dir = "../new/"
 
-source_dir = (Path(__file__).parent/source_dir).resolve()
-dest_dir = (Path(__file__).parent/dest_dir).resolve()
-print(source_dir,dest_dir)
+source_dir = (Path(__file__).parent / source_dir).resolve()
+dest_dir = (Path(__file__).parent / dest_dir).resolve()
+print(source_dir, dest_dir)
 print(type(source_dir))
 
 
@@ -20,4 +21,4 @@ for file_path in source_dir.iterdir():
     # .is_file() ensures you skip subfolders and only target actual files
     if file_path.is_file():
         with Image.open(file_path) as img:
-            img.save(f"{dest_dir}\\{file_path.name.split(".")[0]}.png","png")
+            img.save(f"{dest_dir}\\{file_path.name.split(".")[0]}.png", "png")
