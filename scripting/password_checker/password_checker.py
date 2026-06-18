b80a8ae2c84d8fbb1f13359d81e4d837c93ff33d
Jatin Goyal <goyaljatin344@gmail.com>
2026-06-18 20:50:52 +0530

    password api completed

--- scripting/password_checker/password_checker.py
+++ scripting/password_checker/password_checker.py
@@ -5,6 +5,7 @@ import requests
 def password_api(hashed_pass):
     url = "https://api.pwnedpasswords.com/range/" + hashed_pass
     res = requests.get(url)
+    # print(f"res status code is  :{res.status_code}")
     if res.status_code != 200:
         raise RuntimeError(
             f"Error fetching: {res.status_code}, check the api and try again!"
@@ -14,9 +15,17 @@ def password_api(hashed_pass):
 
 def pass_to_hash(password):
     hashed_pass = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
-    return hashed_pass
+    return hashed_pass[:5], hashed_pass[5:]
 
 
-print(pass_to_hash("password123"))
-print(pass_to_hash("123"))
-# print(pass_to_hash(123))
+def check_if_pawned(password):
+
+    first_hash, tail_hash = pass_to_hash(password)
+    response = password_api(first_hash)
+    for hash, val in (hash.split(":") for hash in response.text.splitlines()):
+        if tail_hash == hash:
+            return val
+    return 0
+
+
+print(f"Password has been pawned: {check_if_pawned("password123")} many times")
