e5df97b2ccef37f2aafa31adb8b78f89dd060ad3
Jatin Goyal <goyaljatin344@gmail.com>
2026-07-11 23:02:12 +0530

    contact form

--- portfolio/server.py
+++ portfolio/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,3 +11,14 @@ def home():
 @app.route("/<string:page_name>")
 def render_page(page_name):
     return render_template(page_name)
+
+
+@app.route("/submit_form", methods=["POST", "GET"])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        # return render_template("thankyou.html")
+        return redirect(url_for("render_page", page_name="thankyou.html"))
+    else:
+        return "Something went wrong. Please try again."
